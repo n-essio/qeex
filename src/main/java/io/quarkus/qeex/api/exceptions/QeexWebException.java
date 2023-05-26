bc87c870f6bff598f5db987c1ed3dc7f19254238
fiorenzo pizza <fiorenzino@gmail.com>
2023-05-26 11:19:14 +0200

    some improuvements

--- src/main/java/io/quarkus/qeex/api/exceptions/QeexWebException.java
+++ src/main/java/io/quarkus/qeex/api/exceptions/QeexWebException.java
@@ -7,7 +7,9 @@ public class QeexWebException extends Exception {
     public String message;        // (4)
     public String language;        // (4)
 
-    public QeexWebException(){}
+    public QeexWebException() {
+    }
+
     public QeexWebException(String projectName,
                             int code,
                             int httpCode,
@@ -19,6 +21,32 @@ public class QeexWebException extends Exception {
         this.httpCode = httpCode;
         this.message = message;
         this.language = language;
+    }
+
+    public static QeexWebException builder(String projectName) {
+        var quex = new QeexWebException();
+        quex.projectName = projectName;
+        return quex;
+    }
+
+    public QeexWebException code(int code) {
+        this.code = code;
+        return this;
+    }
+
+    public QeexWebException httpCode(int httpCode) {
+        this.httpCode = httpCode;
+        return this;
+    }
+
+    public QeexWebException message(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public QeexWebException language(String language) {
+        this.language = language;
+        return this;
     }
 
 
